710be7bcb1bbbaa9cf427932fafc09355b2e27dd
Katarina <katarina.floyd1@gmail.com>
2022-10-24 13:45:18 +0200

    add components and setup router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import './index.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import JobBoard from './Components/JobBoard';
+import JobDetail from './Components/JobDetail';
+import Login from './Components/Login';
+
 function App() {
   return (
     <div className="App">
-      <></>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/jobs' element={<JobBoard />} />
+        <Route path='/job/:id' element={<JobDetail />} />
+      </Routes>
     </div>
   );
 }

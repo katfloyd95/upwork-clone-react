8a4f5948732a05e53b9c7af1339a494cdfab9224
Katarina <katarina.floyd1@gmail.com>
2023-01-26 10:47:49 +0100

    add job board structure

--- src/Components/JobBoard.js
+++ src/Components/JobBoard.js
@@ -1,9 +1,36 @@
 import TopNav from "./TopNav";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 export default function JobBoard() {
   return (
     <>
       <TopNav />
+      <Box
+        sx={{
+          padding: "10%",
+          background: "green",
+          height: "500px",
+          display: "flex",
+        }}
+      >
+        <Box
+          sx={{
+            background: "orange",
+            width: "30%",
+          }}
+        >
+          <Typography variant="h5" gutterBottom sx={{ textAlign: "left" }}>
+            Find Work
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            background: "blue",
+            width: "70%",
+          }}
+        ></Box>
+      </Box>
     </>
   );
 }
